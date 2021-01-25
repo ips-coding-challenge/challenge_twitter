11a8b3f420b96eeff6982b409ff3293877ec0498
ips-coding-challenge <coding_challenge@laposte.net>
2021-01-25 12:22:18 +0100

    Remove index to hashtag since I've already have a unique constraint

--- src/db/migrations/20210124150546_create_hashtags_table.ts
+++ src/db/migrations/20210124150546_create_hashtags_table.ts
@@ -3,7 +3,7 @@ import * as Knex from 'knex'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('hashtags', (t) => {
     t.bigIncrements('id')
-    t.string('hashtag').unique().index()
+    t.string('hashtag').unique()
     t.timestamps(false, true)
   })
 }
